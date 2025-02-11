d15bf3a1bbbe7efe2d70557c0286459c91464858
jake ringate <jakeringate3@gmail.com>
2025-02-10 20:29:19 -0600

    fixed small bugs

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -1,7 +1,9 @@
 const searchGithub = async () => {
   try {
     const start = Math.floor(Math.random() * 100000000) + 1;
-    // console.log(import.meta.env);
+    //console.log('GitHub Token:', import.meta.env.VITE_GITHUB_TOKEN);
+
+     //console.log(import.meta.env.VITE_GITHUB_TOKEN);
     const response = await fetch(
       `https://api.github.com/users?since=${start}`,
       {
@@ -10,15 +12,15 @@ const searchGithub = async () => {
         },
       }
     );
-    // console.log('Response:', response);
     const data = await response.json();
+    //console.log('Data:', data);
     if (!response.ok) {
       throw new Error('invalid API response, check the network tab');
     }
-    // console.log('Data:', data);
+    
     return data;
   } catch (err) {
-    // console.log('an error occurred', err);
+     console.log('an error occurred6', err);
     return [];
   }
 };
@@ -36,7 +38,7 @@ const searchGithubUser = async (username: string) => {
     }
     return data;
   } catch (err) {
-    // console.log('an error occurred', err);
+    console.log('an error occurred', err);
     return {};
   }
 };
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,12 +1,13 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 export interface Candidate {
-  login: string;
-  avatar_url: string;
-  name?: string;
-  location?: string | null;
-  email: string;
-  company?: string | null;
-  bio: string;
-  html_url: string;
+  login: string;  // GitHub username (key used for rejection)
+  avatar_url: string;  // Avatar URL
+  name?: string;  // Full name (optional)
+  location?: string | null;  // Location (optional)
+  email: string;  // Email (optional)
+  company?: string | null;  // Company (optional)
+  bio: string;  // Bio (optional)
+  html_url: string;  // GitHub profile URL
+
 }
   
