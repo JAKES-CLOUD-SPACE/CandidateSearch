c6db654b30451d211f9fa751fa63ef3c1a942139
jake ringate <jakeringate3@gmail.com>
2025-02-11 17:38:31 -0600

    final push

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: './environment',
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+  },
 });
