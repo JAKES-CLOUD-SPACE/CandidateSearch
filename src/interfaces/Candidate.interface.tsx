bc0aade02f6aac48944493797a2477718fe4fa9c
jake ringate <jakeringate3@gmail.com>
2025-02-10 20:21:26 -0600

    added Candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+  login: string;
+  avatar_url: string;
+  name?: string;
+  location?: string | null;
+  email: string;
+  company?: string | null;
+  bio: string;
+  html_url: string;
+}
+  
