dc94102ebd212bdd7ed83548bed62cebbca8d2f4
jake ringate <jakeringate3@gmail.com>
2025-02-10 20:23:00 -0600

    added lots of code to the saved cani file all working right now

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,8 +1,74 @@
+
+import { useState, useEffect } from 'react';
+import { Candidate } from '../interfaces/Candidate.interface';
+
 const SavedCandidates = () => {
+  const [savedCandidates, setSavedCandidates] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    const savedData = localStorage.getItem('savedCandidates');
+    if (savedData) {
+      setSavedCandidates(JSON.parse(savedData));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('savedCandidates', JSON.stringify(savedCandidates));
+  }, [savedCandidates]);
+
+   // Handle the rejection of a candidate (removing them from the list)
+   const handleReject = (username: string) => {
+    const updatedCandidates = savedCandidates.filter(
+      (candidate) => candidate.login !== username
+    );
+    setSavedCandidates(updatedCandidates);
+  };
+
   return (
-    <>
-      <h1>Potential Candidates</h1>
-    </>
+    <div>
+      <h1>Saved Candidates</h1>
+      {savedCandidates.length === 0 ? (
+        <p>No candidates have been accepted.</p>
+      ) : (
+        <table className="candidates-table">
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Location</th>
+              <th>Email</th>
+              <th>Company</th>
+              <th>Bio</th>
+              <th>Reject</th>
+            </tr>
+          </thead>
+          <tbody>
+            {savedCandidates.map((candidate: Candidate) => (
+              <tr key={candidate.login}>
+                <td>
+                  <img
+                    src={candidate.avatar_url}
+                    alt="Avatar"
+                    width={50}
+                    height={50}
+                  />
+                </td>
+                <td>{candidate.login}</td>
+                <td>{candidate.location || 'N/A'}</td>
+                <td>{candidate.email || 'N/A'}</td>
+                <td>{candidate.company || 'N/A'}</td>
+                <td>{candidate.bio || 'N/A'}</td>
+                <td>
+                  <button onClick={() => handleReject(candidate.login)}>
+                    Reject
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 };
 
